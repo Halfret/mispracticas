5f4151b76188f7730d6f5b46887d86fefd4edff8
José Alfredo <soyproylose@gmail.com>
2019-03-19 16:35:56 -0600

    Entrada de nombre
    Metimos nombres, apellidos y edad

---
+++ entradaPersona.py
@@ -0,0 +1,9 @@
+#CAPTURAMOS LA ENTRADA DE TECLADO Y LA GUARDAMOS EN UNA VARIABLE
+nombre= input("ingresar Nombre: ")
+
+apellido= input("ingresar su Apellido: ")
+
+edad= input("ingresar su edad: ")
+
+#Imprimimos lo capturado
+# print(f"Bienvenido {nombre} {apellido}, su edad es: {edad}")
