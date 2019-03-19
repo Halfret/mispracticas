4085ee14bb8e497dbaa175b52c8d5404a1086c3b
José Alfredo <soyproylose@gmail.com>
2019-03-19 16:36:35 -0600

    Entrada de numeros
    Metimos números concatenados y después los sumamos

---
+++ entradaNumero.py
@@ -0,0 +1,7 @@
+numero1= int(input("Ingrese un numero: "))
+numero2= int(input("Ingrese otro numero: "))
+resultado= numero1 + numero2
+print(f"El resultado es: {resultado}")
+
+#Metimos numeros y los sumamos, una vez con input y los concatena, con un int al inicio ya los suma Bienvenido
+#No podemos meter numeros con letras ni decimales con enteros
