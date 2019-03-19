36aed58a0d911f7ca12854a41269de5f42b0380b
José Alfredo <soyproylose@gmail.com>
2019-03-19 17:46:22 -0600

    Ciclos
    Realizamos ciclos, en este caso suma de números.

---
+++ ciclos.py
@@ -0,0 +1,10 @@
+#El while se utiliza es para ciclos para repetir acciones
+#El símbolo ! se utiliza para negación
+#CICLO WHILE
+a= 0
+b= 100
+#Se repite esto infinitamente ya que a sí es menor a b, lo cual puede crashear la PC.
+while(a<b):
+    a=a+1
+    print(a)
+#Ahora se puede realizar el print, ya que hay un límite.
